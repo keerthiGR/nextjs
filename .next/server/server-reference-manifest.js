5428c7dbb37dd12b8206d1da8a10686fe94d17b8
keerthii11Labs <keerthi.guddadahalli@i11labs.com>
2025-10-16 16:17:51 +0530

    somechanges

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WRiOUZAHFFnhy4AorF2f3hg8KlZpHuHp/ThKeeFQg5c=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
